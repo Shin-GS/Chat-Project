bae36f2bec6c9d4e7bae41dfceeacd050787b2cd
DESKTOP-T72GEF9\user <rudtjq0842@naver.com>
2025-06-12 13:36:17 +0900

    feat: HomeApi 추가

---
+++ src/main/java/com/chat/server/controller/HomeApi.java
@@ -0,0 +1,12 @@
+package com.chat.server.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeApi {
+    @GetMapping("/api/health-check")
+    public String healthCheck() {
+        return "health-check";
+    }
+}
