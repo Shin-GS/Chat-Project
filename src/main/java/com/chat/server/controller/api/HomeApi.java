f158903a79ed67bdb983510c2bd17373984dfaf9
DESKTOP-T72GEF9\user <rudtjq0842@naver.com>
2025-06-12 14:01:19 +0900

    refactor: 패키지 구조 변경
    config : 설정
    common : 공통 코드
    domain : entity, repository
    service : 비즈니스 로직
    controller: 컨트롤러

--- src/main/java/com/chat/server/controller/api/HomeApi.java
+++ src/main/java/com/chat/server/controller/api/HomeApi.java
@@ -1,4 +1,4 @@
-package com.chat.server.controller;
+package com.chat.server.controller.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
