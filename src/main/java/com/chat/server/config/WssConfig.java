0bc3122ac0771c68bc873101bd7333b12b0fac6f
DESKTOP-T72GEF9\user <rudtjq0842@naver.com>
2025-07-14 14:02:16 +0900

    build: WebSocketMessageBrokerConfigurer로 변경

--- src/main/java/com/chat/server/config/WssConfig.java
+++ src/main/java/com/chat/server/config/WssConfig.java
@@ -1,16 +1,23 @@
 package com.chat.server.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WssConfig implements WebSocketConfigurer {
+public class WssConfig implements WebSocketMessageBrokerConfigurer {
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(null, "/ws/v1/chat")
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/sub");
+        registry.setApplicationDestinationPrefixes("/pub");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws-stomp")
                 .setAllowedOrigins("*");
     }
 }
